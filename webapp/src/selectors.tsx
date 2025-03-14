35b3c4292c1b680590692e12b43861ee5043d8d3
Jesús Espino <jespinog@gmail.com>
2025-03-14 17:17:13 +0100

    feat: Add selectors for plugin state and translations modal

---
+++ webapp/src/selectors.tsx
@@ -0,0 +1,6 @@
+import {GlobalState} from '@mattermost/types/store';
+import manifest from '@/manifest';
+
+const pluginState = (state: GlobalState): any => state['plugins-' + manifest.id as keyof GlobalState] || {}
+
+export const getTranslationsModalPost = (state: GlobalState): () => void => pluginState(state).translationsModal;
